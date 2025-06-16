8c625a0616e0df0f6e8fecb93f17abdbfe22296d
Daniel Rodriguez <daniel@Daniels-MacBook-Pro-2.local>
2025-06-16 12:39:08 -0500

    Inicios de implementacion Css Bootstrap

--- routes/apiUsuarios.js
+++ routes/apiUsuarios.js
@@ -50,4 +50,64 @@ router.get('/:userId', async function (peticion, respuesta) {
   }
 });
 
+//Metodo para poder agregar un usuario
+router.post('/', async function (peticion, respuesta) {
+  //try catch para controlar si llega a haber un error.
+
+  try {
+    const datos = peticion.body;
+
+    // aca estoy declarando la consulta que me ayudara a mostrar los datos que necesito.
+    const consulta = `INSERT INTO usuarios(nombre, email, contrasena, rol) VALUES (?, ? , ? , ?)`;
+
+    // Consultar el listado de clientes.
+    // aca me retorna un array 
+    await conexionesDb.query(consulta, [datos.nombre, datos.email, datos.contrasena, datos.rol]);
+
+    respuesta.json({
+      //Si se creo voy a mostrar
+      mensaje: 'El usuario creado exitosamente.'
+    });
+  } catch (error) {
+    //si falla voy a mostrar
+    console.error('Error al insertar el usuario', error)
+    // esta respuesta indica que hay un error con el servidor y se va a presentar
+    // en un json con un mensaje de error para evitar que la app se creashee por lo que va a poner el texto.
+    respuesta.status(500).json({
+      error: 'Error al conectar con el servidor'
+    })
+  }
+})
+
+//eliminar... por ahora no lo voy a usar.
+router.delete('/:usuarioId', async function (peticion, respuesta) {
+  //Obtengo el Id del cliente desde los parametros de la URL
+
+  const usuarioId = peticion.params.usuarioId;
+
+  try {
+
+    const consulta = `DELETE FROM usuarios 
+    WHERE usuarios.id = ?`
+
+    await conexionesDb.query(consulta, [usuarioId]);
+
+    respuesta.json({
+      //Si logro eliminar pongo este mensaje 
+      mensaje: 'Usuario eliminado exitosamente.'
+    });
+
+  } catch (error) {
+    console.error('Error en el usuariooo', error);
+
+    respuesta.status(500).json({
+      error: 'Error en el servidor',
+    })
+
+  };
+});
+
+
+
+
 module.exports = router;
